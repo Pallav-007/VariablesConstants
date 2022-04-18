8a164f1a1f12c6f8d8f03a5f861f297b1b9fe970
Pallav-007 <103948779+Pallav-007@users.noreply.github.com>
2022-04-18 21:33:42 +0545

    Add files via upload

---
+++ 03VariablesConstant.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#define PIE 3.14
+/*
+Variables: Something that store value;
+Rules:Cannot start with number,symbol;
+Constants:once declare cannot be changed;
+
+Data Type var_name=value;
+*/
+int main(){
+	int firstnumber=10;
+	printf("The first number is %d\n",firstnumber);
+	printf("The Value Of PIE is %.2f\n",PIE);
+	return 0;
+}
